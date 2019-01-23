a85b48f9a4c639836afcaa71308201fef8ff5bf2
Linnety3 <linoc.veloso@gmail.com>
2019-01-23 16:43:18 -0200

    Training android

--- PrimeiroProjeto/app/src/main/java/primeiroprojeto/linoveloso/com/MainActivity.java
+++ PrimeiroProjeto/app/src/main/java/primeiroprojeto/linoveloso/com/MainActivity.java
@@ -2,6 +2,10 @@ package primeiroprojeto.linoveloso.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View v){
+        TextView textoResultado = (TextView) findViewById(R.id.textoResultado);
+        int numero = new Random().nextInt(10);
+        textoResultado.setText("O numero do resultado é "+ numero);
     }
 }
