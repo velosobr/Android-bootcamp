fd043df1148fb445ff4eba1d731e457ef48dba16
velosobr <linoc.veloso@gmail.com>
2020-08-31 20:25:03 -0300

    WIP - Studying lifecycle

---
+++ DiceRoller/app/src/main/java/com/cursoandroid/diceroller/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.cursoandroid.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var dicesRolled = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textRoll: TextView = findViewById(R.id.roll_text)
+        var buttonRoll: Button = findViewById(R.id.roll_button)
+
+        buttonRoll.setOnClickListener { rollDice(textRoll) }
+    }
+
+    private fun rollDice(text: TextView) {
+
+        if(!dicesRolled){
+            text.text = "The Dice has rolled"
+            dicesRolled = true
+        }else{
+            text.text = "The Dice has already rolled once"
+        }
+
+
+
+    }
+
+
+}
