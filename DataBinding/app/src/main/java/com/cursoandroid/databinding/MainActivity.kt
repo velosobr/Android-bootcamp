3e20a881caabaa87e734c03ceed1a6b4515c724b
velosobr <linoc.veloso@gmail.com>
2020-09-02 21:39:06 -0300

    refactor using databinding concepts

--- DataBinding/app/src/main/java/com/cursoandroid/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/cursoandroid/databinding/MainActivity.kt
@@ -8,25 +8,23 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        done_button.setOnClickListener {
             addNickname(it)
         }
 
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
+        nickname_text.text = nickname_edit.text
+        nickname_edit.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        nickname_text.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
