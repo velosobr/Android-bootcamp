af5aa3b609abd6f7abff1122cc84554a0390e9bd
velosobr <linoc.veloso@gmail.com>
2020-09-04 20:25:42 -0300

    finishing the codelab in databinding

--- DataBinding/app/src/main/java/com/cursoandroid/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/cursoandroid/databinding/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.cursoandroid.databinding
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.cursoandroid.databinding.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Lino Veloso")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        done_button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener {
             addNickname(it)
+
         }
+
+        binding.myName = myName
 
     }
 
     private fun addNickname(view: View) {
-        nickname_text.text = nickname_edit.text
-        nickname_edit.visibility = View.GONE
-        view.visibility = View.GONE
-        nickname_text.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
