104120a7c0ca6fe43363794f94e127ba7c736790
velosobr <linoc.veloso@gmail.com>
2020-10-21 12:39:35 -0300

    Studying clean arch

--- lifecycleandroid/app/src/main/java/com/cursoandroid/lifecycle_android/MainActivity.kt
+++ lifecycleandroid/app/src/main/java/com/cursoandroid/lifecycle_android/MainActivity.kt
@@ -3,11 +3,14 @@ package com.cursoandroid.lifecycle_android
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Chronometer
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val mLiveDataTimerViewModel = LiveDataTimerViewModel by viewModels<>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
